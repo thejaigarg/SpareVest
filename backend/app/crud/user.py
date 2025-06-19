d529e164e6757f42f35799b514bf7eccbc8c59b5
thejaigarg <thejaigarg@gmail.com>
2025-06-19 10:38:15 +0530

    Alembic Setup

--- backend/app/crud/user.py
+++ backend/app/crud/user.py
@@ -1,1 +1,28 @@
 # DB access logic for user.py
+# backend/app/crud/user.py
+
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.schemas.user import UserCreate
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+def create_user(db: Session, user: UserCreate):
+    hashed_pw = pwd_context.hash(user.password)
+    db_user = User(
+        email=user.email,
+        hashed_password=hashed_pw,
+        full_name=user.full_name,
+        is_active=1,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
