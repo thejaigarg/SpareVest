4eb132a066dd86c443a695981bb20033e554d1b9
AkashBahri <bahri.a@northeastern.edu>
2025-07-09 23:45:46 -0400

    Add console log for API_BASE_URL in appConfig.js

--- frontend/src/constants/appConfig.js
+++ frontend/src/constants/appConfig.js
@@ -1,3 +1,4 @@
 // src/constants/appConfig.js
+console.log("❯ API_BASE_URL is:", import.meta.env.VITE_API_BASE_URL);
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
